c498dd883b67eaefd9cd3f42782d76d8ea0a7ba4
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2023-12-05 09:35:07 +0500

    today i learn about Arithmetic Operators here is some example

---
+++ day2.py
@@ -0,0 +1,6 @@
+a = 2;
+b = 4;
+print("the sum of a = ", a , "+ b = ", b , "is: ", a+b)
+print("the Subtraction of a = ", a , "- b = ", b , "is: ", a-b)
+print("the division of a = ", a , "/ b = ", b , "is: ", a//b)
+print("the multiply of a = ", a , "* b = ", b , "is: ", a*b)
