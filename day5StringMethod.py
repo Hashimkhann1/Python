5b05275496762413d88689d36d2a80d4110d2976
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2023-12-06 10:16:17 +0500

    practicing string methods

---
+++ day5StringMethod.py
@@ -0,0 +1,9 @@
+name = "M Hashim khan__--!!"
+
+print(name.upper())
+print(name.lower())
+print(name.rstrip("!-_  "))
+print(name.replace("k" , "K"))
+print(name.count("a"))
+print(name.endswith("!"))
+print(name.find("khan"))
