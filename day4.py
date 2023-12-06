9a21e599ad3b92c992bd8d0638387f0cf3e8bb83
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2023-12-06 10:14:55 +0500

    practice somr pre define function

---
+++ day4.py
@@ -0,0 +1,12 @@
+name = "M Hashim Khan"
+
+# for char in name:
+#     if(char == " "):
+#         print("-")
+#     else:
+#         print(char)
+
+
+print(len(name))
+vname = "Harry"
+print(vname[-4:-2])
