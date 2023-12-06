04a26adee3673a6a6ac6347c54a4b7a6f3cd17b4
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2023-12-06 10:18:42 +0500

    on day 7 i know forloop and how to use for loop

---
+++ day7ForLoop.py
@@ -0,0 +1,18 @@
+color = ['red' , 'green' , 'white' , 'black' , 'blue']
+
+for i in range(len(color)):
+    if(i == len(color) -1):
+        print(color[i],",")
+    else:
+        print(color[i], end=", ")
+
+
+
+name = 'M Hashim'
+for a in name:
+    if(a != " "):
+        print(a , end=" ")
+
+
+# for i in range(1 , 20 , 4):
+#     print(i)
