bcf311484912b3fd3384353dbb591fac608187d1
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2023-12-07 09:15:28 +0500

    how to create function and pass arguments

---
+++ day9Function.py
@@ -0,0 +1,15 @@
+
+
+take_name  = input("What is your name! ")
+
+
+def printName(user_name):
+    print(user_name)
+
+printName(take_name)
+
+def testing(a , b):
+   sumOf = a + b
+   print(sumOf)
+
+testing(6 , 6)
